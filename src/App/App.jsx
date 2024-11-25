260e21f4c28a93aa314b12313371392b9a8b6c5f
Afonso <129227018+Afonso-Front-End@users.noreply.github.com>
2024-11-24 23:17:35 -0300

    Atualizando

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,14 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import Layout from '../Layout/Layout';
 import Streaming from '../Streaming/Streaming';
-
+// import { useNavigate } from 'react-router-dom';
 const App = () => {
-
+  // const navigate = useNavigate();
   return (
+
     <Router>
       <Routes>
         <Route index element={<Layout />} />
-        <Route path='/video/:id/:name' element={<Streaming/>} />
+        <Route path='/video/:id/:name' element={<Streaming />} />
       </Routes>
     </Router>
   )
